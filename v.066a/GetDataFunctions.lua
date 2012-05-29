fbb05e50ca82186e132a459f9cf636493a0c66bc
David Lockwood <dave.r.lockwood@gmail.com>
2012-05-29 13:01:29 -0400

    .

--- v.066a/GetDataFunctions.lua
+++
@@ -1,8 +0,0 @@
-function getNewData()
-    getEveKillData()
-    --testParm={"Deamos","Friggi","Charon","4/20/2011"}
-    --notifyHandle:push("Kill",testParm)
-    
-    --testParm2={"Deamos","Jita","Charon","4/20/2011"}
-    --notifyHandle:push("Kill",tes
-end
